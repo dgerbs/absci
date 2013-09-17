a24529f11bd08da4de9abae48eb9eec538fd033b
Doug Gerber <dojoh@outlook.com>
2013-09-17 19:53:17 -0400

    Delete Migration

--- db/migrate/20130917231602_remove_description_from_antibodies.rb
+++
@@ -1,9 +0,0 @@
-class RemoveDescriptionFromAntibodies < ActiveRecord::Migration
-  def up
-    remove_column :antibodies, :description
-  end
-
-  def down
-    add_column :antibodies, :description, :string
-  end
-end
