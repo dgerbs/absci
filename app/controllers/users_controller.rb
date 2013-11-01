34c8004b0af05c36681262949c8c722156754f5c
dgerbs <dojoh@outlook.com>
2013-11-01 18:11:43 -0400

    Trim UI

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   def show
-  	@user = User.find(params[:id])
-  	@protocols = @user.protocols.paginate(page: params[:page])
+    @user = User.find(params[:id])
+    @protocols = @user.protocols.paginate(page: params[:page])
   end
 end
