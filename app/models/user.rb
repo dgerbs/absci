265885b620f4fb58a74a669c131263f79aa4ea8e
dgerbs <dojoh@outlook.com>
2014-01-06 12:14:08 -0500

    Edit associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,7 @@ class User < ActiveRecord::Base
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me, :name, :image
 
-  has_many :antibodies
+  # has_many :antibodies
   has_many :protocols, dependent: :destroy
   mount_uploader :image, ImageUploader
 end
